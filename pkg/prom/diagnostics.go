0c211c08c20844ade7a5fc774679144b5bfee347
Ishaan Mittal <ishaanmittal123@gmail.com>
2024-11-25 21:26:45 +0530

    add comment for more clarity
    Signed-off-by: Ishaan Mittal <ishaanmittal123@gmail.com>

--- pkg/prom/diagnostics.go
+++ pkg/prom/diagnostics.go
@@ -78,6 +78,7 @@ var diagnosticDefinitions map[string]*diagnosticDefinition = map[string]*diagnos
 		Label:       "Kubecost metrics available",
 		Description: "Determine if metrics from Kubecost are available during last 5 minutes.",
 	},
+	// Commented out because it is not critical to Opencost metrics and can cause confusion with false alarms about node-exporter
 	// NodeExporterDiagnosticMetricID: {
 	// 	ID:          NodeExporterDiagnosticMetricID,
 	// 	QueryFmt:    `absent_over_time(node_cpu_seconds_total{%s}[5m] %s)`,
